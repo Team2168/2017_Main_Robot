6e849163efc029bc6d4a4a3d96ed7f6062d776c0
Cierra <cc362x@gmail.com>
2018-01-12 20:34:33 -0500

    Command to start left and score at the right scale

---
+++ src/org/team2168/commands/auto/StartingLeftScoringRightScale.java
@@ -0,0 +1,23 @@
+package org.team2168.commands.auto;
+
+import org.team2168.commands.drivetrain.ShiftHigh;
+import org.team2168.commands.drivetrain.PIDCommands.DriveXDistance;
+import org.team2168.commands.drivetrain.PIDCommands.RotateXDistancePIDZZZ;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+
+public class StartingLeftScoringRightScale extends CommandGroup {
+
+	public StartingLeftScoringRightScale() {
+    	 addSequential(new DriveXDistance(4,0.7,0.1));
+    	 addSequential(new RotateXDistancePIDZZZ(-30,0.7,0.2));
+    	 addSequential(new DriveXDistance(6,0.7,0.1));
+    	 addSequential(new RotateXDistancePIDZZZ(30,0.7,0.2));
+    	 addSequential(new DriveXDistance(6,0.7,0.1));
+    	 addSequential(new RotateXDistancePIDZZZ(90,0.7,0.2));
+    	 addSequential(new DriveXDistance(15,0.7,0.1));
+    	 addSequential(new RotateXDistancePIDZZZ(-90,0.7,0.2));
+    	 
+    }
+}
